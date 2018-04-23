e36931b0831b545da0f99fd433121998c7d02956
Matthias Kunnen <matthias.kunnen@gmail.com>
2018-04-23 16:27:26 +0200

    Created publish script and flattened library

---
+++ publish.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+echo "What type of publish?"
+select version_type in "patch" "minor" "major"; do
+    if [ -z "$version_type" ]; then
+        echo "Invalid, enter type number or CTRL + C to quit."
+        continue
+    fi
+
+    read -p "Creating commit and tag for a $version_type release. Press [Enter].";
+
+    # Use npm to increment the version and capture it
+    version=`npm version ${version_type} --git-tag-version=false` || exit "$?"
+
+    # Get last tag
+    last_tag=`git describe --abbrev=0 2>/dev/null`
+
+    if [ "$?" -ne  "0" ]; then
+        echo "There is no previous tag, assuming first publication"
+        last_tag=`git rev-list --max-parents=0 HEAD` || exit "$?"
+    fi
+
+    changelog=`git log --format="- %s%+b" ${last_tag}..HEAD`
+    message="Bumped package version to $version"
+
+    echo "Message:"
+    echo -e "$message"
+    read -p "Examine message. [Enter] to continue"
+
+    echo ""
+    echo "Changelog:"
+    echo -e "$changelog"
+    read -p "Examine changelog. [Enter] to continue"
+
+    git add package.json package-lock.json || exit "$?"
+    git commit -m "$message" || exit "$?"
+
+    tag_args=(
+        -a
+        -m "Released $version"
+        -m "$changelog"
+    )
+
+    signing_key=`git config --get user.signingKey`
+
+    if [ -n "$signing_key" ]; then
+        echo "Found key, signing..."
+        tag_args+=(-s)
+    fi
+
+    git tag "${version}" "${tag_args[@]}" || exit "$?"
+
+    echo "Building"
+    npm run build || exit "$?"
+    cp ./package.json ./.npmignore ./dist || exit "$?"
+    cd dist || exit "$?"
+
+    read -p "Ready to publish?; [Enter] to continue";
+    npm publish || exit "$?"
+    break
+done
