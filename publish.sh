ca4563ca878067f49203f376a5df2008d5f4c4ee
Matthias Kunnen <matthias.kunnen@gmail.com>
2019-10-25 13:40:46 +0000

    Use lib folder in publish and use whitelisting
    Whitelisting prevents accidental publishing of files in comparison to
    blacklisting with .npmignore.

--- publish.sh
+++ publish.sh
@@ -52,13 +52,7 @@ select version_type in "patch" "minor" "major"; do
 
     git tag "${version}" "${tag_args[@]}" || exit "$?"
 
-    rm -Rf dist
-    echo "Building"
-    npm run build || exit "$?"
-    cp ./package.json ./.npmignore ./README.md ./dist || exit "$?"
-    cd dist || exit "$?"
-
-    read -p "Ready to publish?; [Enter] to continue";
+    read -p "Ready to build and publish?; [Enter] to continue";
     npm publish || exit "$?"
     break
 done
